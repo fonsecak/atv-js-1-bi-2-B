3a1eab7dc8518fda09718a08b04cf610f5ebb5ab
Gabriel Fonseca <66262553+fonsecak@users.noreply.github.com>
2024-09-06 19:46:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const formInputNome = document.getElementById('nome');
 const formInputEmail = document.getElementById('email');
 
 function sendTaks () {
-    if (formInputNome.value.length === 0 || formInputEmail.value.length === 0 ){
+    if (formInputNome.value === "" || formInputEmail.value === "" ){
         alert ("Preencha todos os campos");
     }
         
